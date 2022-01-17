b66e75b9faadef0ab0a669bb85a285019120dfd2
coderlei <972761675@qq.com>
2022-01-17 17:07:49 +0800

    study: 父子组件通信(1)/对props进行约束以及设置默认值

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from "prop-types"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  /**
+   * 作用：定义props的默认值
+   * 该方式等同于下面的写法：
+   * App.defaultProps = { name: 'coderlei' }
+   */
+  static defaultProps = {
+    name: 'coderlei'
+  }
+  // 对props的属性进行约束
+  static propTypes = {
+    name: PropTypes.string.isRequired
+  }
+  render() {
+    // 1. props是在父类定义的 但是挂载还是挂载到了子类上
+    // 2. 构造函数默认就执行了super(props) 如果在构造函数中没有别的操作 可以省略构造函数的声明
+    const { name } = this.props
+    return (
+      <div className="App">
+        <h2>{name}</h2>
+      </div>
+    );
+  }
+  // 组件卸载前执行的钩子
+  componentWillUnmount() { }
+  // 组件挂载后执行的钩子
+  componentDidMount() { }
+  // 组件更新后执行的钩子
+  componentDidUpdate() { }
+  // 决定render函数是否需要重新渲染
+  shouldComponentUpdate() { }
+  // 获取更新前的数据(快照)
+  getSnapshotBeforeUpdate() { }
 }
 
 export default App;
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,5 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// 此文件是测试初始化文件

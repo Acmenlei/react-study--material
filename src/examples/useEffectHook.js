be66156d88e4d1ab7ba34912d6b9883fb744eb8e
coderlei <972761675@qq.com>
2022-01-24 22:19:03 +0800

    last push

--- src/examples/useEffectHook.js
+++ src/examples/useEffectHook.js
@@ -4,7 +4,10 @@ export default function EffectHook() {
     const [isLogin, setIsLogin] = useState(true);
     // 第二个参数是为effect性能优化所设计
     useEffect(() => {
-        console.log("只有当依赖的isLogin数据发生了变化 才会再次执行useEffect")
+        console.log("组件挂载，只有当依赖的isLogin数据发生了变化，才会再次执行useEffect")
+        return () => {
+            console.log("组件卸载")
+        }
     }, [isLogin])
     return <div>
         {isLogin ? <div>登录</div> : <div>未登录</div>}
